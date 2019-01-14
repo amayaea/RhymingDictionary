c6b1b1f37915cfd902146b6f784adee84a36c900
amayaea <emilio.a.amaya@vanderbilt.edu>
2019-01-14 02:18:07 -0600

    fixed comments and output format

--- src/RhymingDictionary.java
+++ src/RhymingDictionary.java
@@ -18,21 +18,26 @@ public class RhymingDictionary {
     }
 
     /**
-     * Prompts the user to enter a word and will return the address to the rhyming
-     * database of that word
+     * Prompts the user to enter a word
+     * @return String representing address to the rhyming database of that word
      */
     private static String getWord() {
         Scanner console = new Scanner(System.in);
-        System.out.println("Enter Word: ");
+        System.out.print("Enter Word: ");
         String word = console.next();
         return "https://www.rhymedb.com/word/" + word;
     }
 
+    /**
+     * Prompts the user to determine how the results should be sorted
+     * @return String representing the kind of sorting the user specified
+     */
     private static String getSorting(){
         Scanner console = new Scanner(System.in);
-        System.out.println("How would you like the words to be sorted?");
-        System.out.println("Alphabetically or by syllable count? (A|S):");
+        System.out.println("\nHow would you like the words to be sorted?");
+        System.out.print("Alphabetically or by syllable count? (A|S): ");
         String sort = console.next().toUpperCase();
+
         while (!(sort.equals("A") || sort.equals("S"))) {
             System.out.println("Please select A or S");
             sort = console.next();
@@ -42,7 +47,8 @@ public class RhymingDictionary {
 
 
     /**
-     * Gets the rhyming words from the rhyming database and sorts them alphabetically
+     * Gets the rhyming words from the rhyming database
+     * @return ArrayList of all the rhyming words
      */
     private static ArrayList<String> getRhymes(String website) throws IOException {
         Document doc = Jsoup.connect(website).get();
